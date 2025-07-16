250973562b19bc7f4a50283debc2dcb612209e65
solah5 <solahslh89@gmail.com>
2025-07-16 20:12:25 +0700

    feat: Serve static files from database file

--- src/database.js
+++ src/database.js
@@ -1,10 +1,19 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "notes_app",
+  // Gunakan NAMA variabel yang diberikan Railway
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT, // Penulisan port Anda sudah benar!
+
+  // Tambahkan konfigurasi SSL untuk koneksi aman ke database cloud
+  ssl: {
+    "rejectUnauthorized": true
+  },
+
+  // Opsi ini bisa tetap ada
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
